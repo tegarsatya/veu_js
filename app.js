97153b1984d1aa1fdd0698a0e7795077069e4b0a
Backend Server <backendserver@gmail.com>
2020-05-27 09:22:16 +0700

    pertama

---
+++ app.js
@@ -0,0 +1,28 @@
+var dataProduct = {
+    products:[
+        {
+        "name": "Bambo Thermal Ski Coat",
+        "description":"Baju Ini Termasuk baju yang bagus ",
+        "price":"99",
+        "image":"https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg"
+        },
+        {
+        "name": "Bambo Thermal Ski Coat",
+        "description":"Baju Ini Termasuk baju yang bagus ",
+        "price":"99",
+        "image":"https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg"
+        },
+        {
+        "name": "Bambo Thermal Ski Coat",
+        "description":"Baju Ini Termasuk baju yang bagus ",
+        "price":"99",
+        "image":"https://hplussport.com/wp-content/uploads/2016/12/ski-coat_LYNDA_29940.jpg"
+        }
+    ]
+ }
+
+var app = new Vue({
+    el: '#app',
+    data:dataProduct
+});
+
